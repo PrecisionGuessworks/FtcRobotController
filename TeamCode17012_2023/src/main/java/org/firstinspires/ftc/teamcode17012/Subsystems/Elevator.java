f243d911123ee1c3315948d4b0dd982674ab4645
Jill Rubacha <jrubacha@lsc.k12.in.us>
2022-11-29 08:19:16 -0500

    added 2023 game specific subsystem templates for students to complete

---
+++ TeamCode17012_2023/src/main/java/org/firstinspires/ftc/teamcode17012/Subsystems/Elevator.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode17012.Subsystems;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class Elevator {
+    Telemetry telemetry;
+
+    // TODO: Declare the motor for the elevator. This will be running of a SPARKmini motor contorller. As such, it must be considered a CRServo in the code
+
+    public Elevator(HardwareMap hardwareMap, Telemetry telemetry) {
+        this.telemetry = telemetry;
+
+        // TODO: Use hardwareMap.get to setup the elevator motor
+        // TODO: Use setDirection to set up the direction of elevator function (Up "Forwards", Down "Backwards")
+        // TODO: Use setZeroPowerBehavior to set motor to brake mode
+    }
+
+    // TODO: Write method for running the elevator
+    // TODO: Write method for stopping elevator motion
+
+    // TODO: (Long term) Write method to freeze elevator in place (reading encoder and making sure it doesn't slip)
+}
---
+++ TeamCode17012_2023/src/main/java/org/firstinspires/ftc/teamcode17012/Subsystems/Gripper.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode17012.Subsystems;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class Gripper {
+    Telemetry telemetry;
+
+    // TODO: Declare the servo(s) used for the gripper.
+
+    public Gripper(HardwareMap hardwareMap, Telemetry telemetry) {
+        this.telemetry = telemetry;
+
+        // TODO: USe hardwareMap.get for each servo created above
+    }
+
+    // TODO: Write method to close the gripper
+    // TODO: Write method to open the gripper
+}
