01017d0f108e07f5b1529aadde421c425e1611ab
Jill Rubacha <jrubacha@lsc.k12.in.us>
2022-12-17 09:12:26 -0500

    created debug teleop to test motors individually and not mess up the regular teleop

---
+++ TeamCode17012_2023/src/main/java/org/firstinspires/ftc/teamcode17012/DebugMode.java
@@ -0,0 +1,124 @@
+package org.firstinspires.ftc.teamcode17012;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode17012.Subsystems.BotUtilities;
+import org.firstinspires.ftc.teamcode17012.Subsystems.Camera;
+import org.firstinspires.ftc.teamcode17012.Subsystems.MecanumDrivetrain;
+import org.firstinspires.ftc.teamcode17012.Subsystems.NavX;
+
+
+//////////////////////////////////////////////////////////////////////////////////////////
+@TeleOp(name="Debug Mode", group="Mecanum")
+//@Disabled        // Comment/Uncomment this line as needed to show/hide this opmode
+//////////////////////////////////////////////////////////////////////////////////////////
+
+public class DebugMode extends OpMode {
+    private ElapsedTime runtime = new ElapsedTime();
+    MecanumDrivetrain drivetrain;
+    BotUtilities utilities;
+    NavX imu;
+    Camera camera;
+
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
+    /* Code to run ONCE when the driver hits INIT */
+    @Override
+    public void init() {
+        drivetrain = new MecanumDrivetrain(this.hardwareMap, this.telemetry);
+        utilities = new BotUtilities(this.telemetry);
+        camera = new Camera(this.hardwareMap, this.telemetry);
+
+        // Set up our telemetry dashboard
+        getTelemetry();
+
+        // Tell the driver that initialization is complete.
+        telemetry.addData("Status", "Initialized    \n17012 is ready to play.\n\n:)");
+    }//
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
+    /* Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY */
+    @Override
+    public void init_loop() {
+        telemetry.addData("tag", camera.getCurrentTag());
+        telemetry.update();
+    }
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
+    /* Code to run ONCE when the driver hits PLAY */
+    @Override
+    public void start() {
+        runtime.reset();
+        getTelemetry();
+    }
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
+    /* Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP */
+    @Override
+    public void loop() {
+        checkDriverController();
+        checkOperatorController();
+
+        // Call Telemetry
+        getTelemetry();
+
+    }
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
+    /* Code to run ONCE after the driver hits STOP */
+    @Override
+    public void stop() {
+        telemetry.addData("Robot Stopped. ", "Have a nice day.");
+        telemetry.addData("Final runtime: ", runtime.toString());
+        telemetry.update();
+    }
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
+    /*                              TELEOP-SPECIFIC METHODS                                 */
+//////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
+
+    public void checkDriverController() {
+        telemetry.addLine("Updating DriverControl");
+
+        // Drive Control
+        //drivetrain.mecanumDrive_Cartesian(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x);
+
+        // Single Button motor control
+        if (gamepad1.triangle) {
+            drivetrain.setMotorPower(1,0,0,0);
+        } else if (gamepad1.circle) {
+            drivetrain.setMotorPower(0,1,0,0);
+        } else if (gamepad1.square) {
+            drivetrain.setMotorPower(0,0,1,0);
+        } else if (gamepad1.cross) {
+            drivetrain.setMotorPower(0, 0, 0, 1);
+        } else {
+            drivetrain.setMotorPower(0,0,0,0);
+        }
+    }
+
+    public void checkOperatorController(){
+        // TODO: Add any controls of operation for the operator
+    }
+
+    public void getTelemetry() {
+        // Show the elapsed game time
+        telemetry.addData("Run Time: ", runtime.toString());
+
+        // Telemetry about motion
+        //telemetry.addData("Motors", "leftFront (%.2f), rightFront (%.2f), rightRear (%.2f), leftRear (%.2f)", telemValues[0], telemValues[1], telemValues[2], telemValues[3]);
+        telemetry.update();
+    }  // getTelemetry
+
+}    // The Almighty Curly Brace For Everything
+
