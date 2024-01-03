95ae1204d036710228654a245490a2b6b8041106
jrubacha <jrubacha@lsc.k12.in.us>
2024-01-03 13:16:46 -0500

    constant fix

--- TeamCode18638_2024/src/main/java/org/firstinspires/ftc/teamcode18638/Constants.java
+++ TeamCode18638_2024/src/main/java/org/firstinspires/ftc/teamcode18638/Constants.java
@@ -5,13 +5,12 @@ public class Constants {
     public static final double DRIVE_SPEED_MODIFIER = 0.85;
 
     // Gripper Constants
-    public final double OPEN_POSITION = 0.0;
-    public final double CLOSED_POSITION = 1.0;
+    public static final double CLOSED_POSITION = 1.0;
 
     // Wrist Constants
-    public final double GRIPPER_STOWED_POSITION = 1.0;
-    public final double GRIPPER_DEPLOYED_POSITION = 0.0;
+    public static final double GRIPPER_STOWED_POSITION = 1.0;
+    public static final double GRIPPER_DEPLOYED_POSITION = 0.0;
 
     // Arm Constants
-    public final double ARM_ROTATION_SPEED = 0.75;
+    public static final double ARM_ROTATION_SPEED = 0.75;
 }
